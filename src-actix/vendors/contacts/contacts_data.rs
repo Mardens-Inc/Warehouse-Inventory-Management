2cd9bc68da6fb7b8117ed0b5bde6f61ed167722f
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-04-16 12:46:40 -0400

    Add primary key and optional timestamps to ContactData
    Marked `id` as the primary key and added `updated_at` field to the struct. Changed `created_at` to be optional for better flexibility. These changes improve database interaction and schema compatibility.

--- src-actix/vendors/contacts/contacts_data.rs
+++ src-actix/vendors/contacts/contacts_data.rs
@@ -1,10 +1,11 @@
 use serde_hash::HashIds;
-use sqlx::types::chrono;
 
 #[derive(HashIds, sqlx::FromRow)]
 pub struct ContactData {
     #[hash]
+    #[sqlx(primary_key)]
     pub id: u32,
+    #[hash]
     pub vendor_id: u32,
     pub first_name: String,
     pub last_name: String,
@@ -12,5 +13,6 @@ pub struct ContactData {
     pub phone: Option<String>,
     pub address: Option<String>,
     pub notes: Option<String>,
-    pub created_at: chrono::DateTime<chrono::Utc>,
+    pub created_at: Option<chrono::DateTime<chrono::Utc>>,
+    pub updated_at: Option<chrono::DateTime<chrono::Utc>>,
 }
